0cdda790c7432d89d094c1c50d010714eb6abaf9
Abhishek Mazumder <abhishek.mazumder@beteasy.com.au>
2018-10-19 15:55:24 +1100

    Implemented Webpack.

--- server/index.jsx
+++ server/index.jsx
@@ -1,9 +1,30 @@
 import express from 'express';
+import webpack from 'webpack';
 import yields from 'express-yields';
 import fs from 'fs-extra';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+    const config = require('../webpack.config.dev.babel.js').default;
+    const compiler = webpack(config);
+
+    app.use(require('webpack-dev-middleware')(compiler,{
+        noInfo: true,
+        stats: {
+            assets: false,
+            colors: true,
+            version: false,
+            hash: false,
+            timings: false,
+            chunks: false,
+            chunkModules: false
+        }
+    }));
+
+    app.use(require('webpack-hot-middleware')(compiler));
+}
 
 app.get(['/'], function * (req,res){
     let index = yield fs.readFile('./public/index.html','utf-8');
